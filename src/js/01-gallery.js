0ed38274514beecec8a3379306c83eb675c2c449
Lisa Riabokon <lisa.checheleva@gmail.com>
2023-02-28 17:40:13 +0200

    task 1 gallery with simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// 1.Стоворюємо розмітку i рндеримо її в дів
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__link" href="${original}">
+      <img src="${preview}" 
+      alt="${description}" 
+      title="${description}"/></a>
+    `;
+    })
+    .join('');
+}
+//  ---------------------------------------
+
+// Додаємо слухача кліків
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+  // Відміна завантаження за замовчуванням
+  evt.preventDefault();
+
+  // Перевірка, що клік на зображенні
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+// підключення модального вікна з затримкою опису в 250мс
+new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
